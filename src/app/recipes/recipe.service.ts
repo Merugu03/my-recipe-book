a4571b97200d81bafae958e8f2f9b628be76cc90
Merugu03 <meruguakshay@gmail.com>
2021-03-21 00:11:55 +0530

    Managreing recipes in recipe service

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,16 @@
+import { Recipe } from "./recipe.model";
+
+export class RecipeService{
+    private recipes:Recipe[]=[
+        new Recipe("A Test Reipe","This is a simply test",
+        "https://www.modernhoney.com/wp-content/uploads/2018/03/The-Best-Chicken-Marinade-Recipe-1024x730.jpg"),
+        new Recipe("Second Test","Simply testing",
+        "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-crispy-chicken-thighs-garlic-rosemary-1567793052.png?crop=1.00xw:0.755xh;0,0.0192xh&resize=980:*"),
+        new Recipe("Biryani ","Tesing it","https://144f2a3a2f948f23fc61-ca525f0a2beaec3e91ca498facd51f15.ssl.cf3.rackcdn.com/uploads/food_portal_data/recipes/recipe/hero_article_image/3968/b70f9551dc0a9e4b8742/compressed_chickenrice.jpg")
+      ];
+      getRecipe()
+      {
+          return this.recipes.slice();
+      }
+    
+}
--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipes-list',
@@ -8,20 +9,15 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipesListComponent implements OnInit {
   @Output() recipeWasSelected= new EventEmitter<Recipe>();
-  recipes:Recipe[]=[
-    new Recipe("A Test Reipe","This is a simply test",
-    "https://www.modernhoney.com/wp-content/uploads/2018/03/The-Best-Chicken-Marinade-Recipe-1024x730.jpg"),
-    new Recipe("Second Test","Simply testing",
-    "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-crispy-chicken-thighs-garlic-rosemary-1567793052.png?crop=1.00xw:0.755xh;0,0.0192xh&resize=980:*"),
-    new Recipe("Biryani ","Tesing it","https://144f2a3a2f948f23fc61-ca525f0a2beaec3e91ca498facd51f15.ssl.cf3.rackcdn.com/uploads/food_portal_data/recipes/recipe/hero_article_image/3968/b70f9551dc0a9e4b8742/compressed_chickenrice.jpg")
-  ];
-  constructor() { }
-
+  recipes:Recipe[];
+  constructor(private recipeService:RecipeService) { }
   ngOnInit(): void {
+    this.recipeService.getRecipe();
   }
   onRecipeSelected(recipe: Recipe)
   {
     this.recipeWasSelected.emit(recipe);
   }
+
 
 }
